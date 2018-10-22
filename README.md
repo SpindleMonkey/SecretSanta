d12e3dac9acf1d7521665baad1dcaf73518b765d
connie kephart <spindle.monkey@gmail.com>
2018-10-22 09:55:53 -0600

    Updated README; fixes for part 1; initial commit of part 2

--- README.md
+++ README.md
@@ -3,4 +3,23 @@ Secret Santa generator
 
 This is a coding exercise in three parts, and is defined [here](MLB_Coding_Exercise.pdf).
 
-My solution can be found at [www.constantlykephart.com/MLB](www.constantlykephart.com/MLB).
+My solution can be found at [www.constantlykephart.com/MLB](www.constantlykephart.com/MLB/).
+
+## Environment
+This was tested in Firefox on a Mac. No testing was done on any other platform, but I expect most browsers will work; no promises tablets/phones will be usable.
+
+## Overview
+I wrote this in JavaScript since that's the most recent language I've been working with. This wasn't the best language choice because there was quite a bit of overhead needed (HTML, CSS) to run the solutions in the browser. I'd pick another language if I were to do this over.
+
+I hard-coded the families I used for testing due to the time constraint. Ideally, my solution would allow for an input file, and would produce a file as output (or even individual emails/texts so nobody knows all of the assignments!).
+
+## Part 1
+My solution for part 1 is a simple shifting of the family list. The shift, or offset, is randomly generated so it's possible you'd get different assignments from year to year. Since a shift of 0 is not allowed, you don't have to worry about anybody being assigned themself (unless, of course, the family grows, and then you should use the Part 2 solution!).
+
+## Part 2
+In order to not duplicate Secret Santa assignments more frequently than every 3 years, I added a history array to each family member to track the previous 3 years' partners. I also switched to randomly generated assignments. 
+
+Known issue: Occasionally, I can't make an assignment because all of the possible partners have been assigned. I need to add some code to work back through the current Secret Santa/partner assignments and find somebody who can swap partners with the current Secret Santa, but until that fix is made, I assign the first available partner (meaning the 3-year rule and/or being your own partner rule is broken); watch the console for these occcurences!
+
+## Part 3
+Since I promised my solution by the end of the weekend, and I had definitely used more than 2-3 hours on parts 1 and 2, I don't have a completed part 3. My plan is to take part 2, add another field to each of the family members to show immediate family members, and then modify findPartner() from part 2 to check that the selected partner is not part of the immediate family. I expect there will be issues with not being able to assign everybody a partner, and I'm pretty sure I'll have to re-think how I use data structures and the existing assignments to adjust partner assignments.
--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-console.log( 'js is loaded' );
+// console.log( 'js is loaded' );
+
+/* ********************************************************* */
 
 /**
  * Part 1 Data
  */
 
-var families = [ 
+let families = [ 
   [
     'Connie', 
     'Al', 'Mona',
@@ -16,12 +18,80 @@ var families = [
     'Connie', 'Laura', 'Greg'
   ],
   [
-    'Connie'
+    'Daisy'
   ],
   [
     'Connie', 'Laura'
   ]
 ];
+
+/* ********************************************************* */
+
+/**
+ * Part 2 Data
+ */
+
+let familiaSize = 0;
+
+let results;
+
+let familias = [ 
+  [ /* each member has an array to hold past Secret Santa partners (partner history) */
+    { santa: 'Connie',    avail: true, partner: '', history: [] },
+    { santa: 'Al',        avail: true, partner: '', history: [] },
+    { santa: 'Mona',      avail: true, partner: '', history: [] },
+    { santa: 'Geoff',     avail: true, partner: '', history: [] },
+    { santa: 'Anya',      avail: true, partner: '', history: [] },
+    { santa: 'Monique',   avail: true, partner: '', history: [] },
+    { santa: 'Julie',     avail: true, partner: '', history: [] },
+    { santa: 'Christine', avail: true, partner: '', history: [] },
+    { santa: 'Tyson',     avail: true, partner: '', history: [] },
+    { santa: 'Alexandra', avail: true, partner: '', history: [] },
+    { santa: 'Dan',       avail: true, partner: '', history: [] },
+    { santa: 'Isaak',     avail: true, partner: '', history: [] },
+    { santa: 'Elliot',    avail: true, partner: '', history: [] },
+    { santa: 'Chris',     avail: true, partner: '', history: [] },
+    { santa: 'Lynn',      avail: true, partner: '', history: [] },
+    { santa: 'Andrea',    avail: true, partner: '', history: [] },
+    { santa: 'Laura',     avail: true, partner: '', history: [] }
+  ],
+  [
+    { santa: 'Connie',    avail: true, partner: '', history: [] },
+    { santa: 'Byrdie',    avail: true, partner: '', history: [] },
+    { santa: 'Annie',     avail: true, partner: '', history: [] },
+    { santa: 'Oskar',     avail: true, partner: '', history: [] },
+    { santa: 'Zelda',     avail: true, partner: '', history: [] },
+    { santa: 'Olivia',    avail: true, partner: '', history: [] }
+  ],
+  [
+    { santa: 'Connie',    avail: true, partner: '', history: [] },
+    { santa: 'Laura',     avail: true, partner: '', history: [] },
+    { santa: 'Greg',      avail: true, partner: '', history: [] }
+  ],
+  [
+    { santa: 'Connie',    avail: true, partner: '', history: [] }
+  ]
+];
+
+/* ********************************************************* */
+
+/**
+ * Part 3 Data
+ */
+
+let familjer = [
+    { santa: 'Al',        avail: true, partner: '', history: [], immFam: ['Mona', 'Geoff'] },
+    { santa: 'Mona',      avail: true, partner: '', history: [], immFam: ['Al', 'Geoff'] },
+    { santa: 'Geoff',     avail: true, partner: '', history: [], immFam: ['Al', 'Mona', 'Anya', 'Monique'] },
+    { santa: 'Anya',      avail: true, partner: '', history: [], immFam: ['Geoff'] },
+    { santa: 'Monique',   avail: true, partner: '', history: [], immFam: ['Geoff'] },
+];
+
+/* ********************************************************* */
+
+/**
+ * Part 2 Data
+ */
 
 /**
  * Common code for all parts of the Secret Santa Generator
@@ -44,11 +114,11 @@ function getRandom(min, max) {
  */
 
 function assignSecretSantas(family) {
-  let results = document.getElementById('family' + (family + 1));
+  let results = document.getElementById('family' + (family + 1) );
 
   /* check for a couple of easy cases first */
   if ( families[family].length == 1 ) {
-    results.textContent = "You are your own Secret Santa! Don't go too crazy, ok?";
+    results.textContent = "You're your own Secret Santa, " + families[family][0] + "! Don't go too crazy, ok?";
   } else {
     /** 
      * We have at least 2 people to work with, so we'll use a random 
@@ -87,5 +157,129 @@ function assignSecretSantas(family) {
 
     resultsString += '</ul>';
     results.innerHTML = resultsString;
+  }
+}
+
+/* ********************************************************* */
+
+/**
+ * Part 2 specific code
+ */
+
+function initAvailability( family ) {
+  for ( let i = 0; i < familiaSize; i++ ) {
+    familias[family][i].avail = true;
+  }
+}
+
+function findPartner( family, ssanta ) {
+  /* new partner needs to be available, and not assigned in the last 3 years */
+  let attempts = familiaSize * 5; // let's avoid infinite loops if we can
+  let partner = getRandom(0, familiaSize - 1);
+
+  /**
+   * don't assign yourself as your Secret Santa
+   * assign only available partners
+   * don't assign somebody from the last 3 years
+   */
+  while ( ( ( ssanta == partner ) ||
+          ( familias[family][partner].avail == false ) ||
+          ( familias[family][ssanta].history.indexOf( familias[family][partner].santa ) != -1 ) ) &&
+          ( attempts > 0 ) ) {
+    partner = getRandom(0, familiaSize - 1);
+    attempts--;
+  }
+
+  if ( attempts == 0 ) {
+    /**
+     * TODO: need to go back through the already-assigned partners and 
+     * find one that can swap with this ssanta; for now, we'll just
+     * assign the first available partner even though we're breaking
+     * either the 3-year rule or the rule to not assign somebody as
+     * their own Secret Santa because we need to make some kind of
+     * assignment.
+     */
+    partner = 0;
+    while ( familias[family][partner].avail == false ) {
+      partner++;
+    }
+    console.log( 'no match for ' + ssanta + '; using first avail ' + partner );
+  }
+
+  return partner;
+}
+
+function assignPartner( family, ssanta, partner ) {
+  /* assign the partner, mark unavailable */
+  familias[family][ssanta].partner = familias[family][partner].santa;
+  familias[family][partner].avail = false;
+}
+
+function cleanupHistory( family ) {
+  /* make all partner assignments official by adding to history */ 
+  for ( let i = 0; i < familiaSize; i++ ) {
+    familias[family][i].history.push(familias[family][i].partner );
+    if ( familias[family][i].history.length > 3 ) {
+      /* we only want the last 3 years' history, so get rid of the oldest entry */
+      familias[family][i].history.shift();
+    }
+  }
+}
+
+function showSecretSantaAssignments( family ) {
+  let resultsString = 'There are ' + familiaSize + ' members in this family.';
+  resultsString += '<ul>';
+
+  for ( let i = 0; i < familiaSize; i++ ) {
+    resultsString += '<li>' + 
+      familias[family][i].santa + ' is ' + 
+      familias[family][i].partner + "'s Secret Santa; history: " + 
+      familias[family][i].history + 
+      '</li>';
+  }
+
+  resultsString += '</ul>';
+
+  results.innerHTML = resultsString;
+}
+
+function generateSecretSantas(family) {
+  results = document.getElementById('family2' + (family + 1));
+
+  familiaSize = familias[family].length;
+
+  initAvailability( family );
+
+  /* check for a couple of easy cases first */
+  if ( familiaSize == 1 ) {
+    results.textContent = "You're your own Secret Santa, " + 
+      familias[family][0].santa + "! Don't go too crazy, ok?";    
+  } else if ( familiaSize < 4 ) {
+    results.textContent = "You need at least 4 people in your family so \
+      Secret Santa's don't repeat more than once every 3 years (your family \
+      has only " + familiaSize + " members).";
+  } else {
+
+    /** 
+     * We have at least 4 people to work with! We'll assign all Secret
+     * Santas using random numbers this time around because now we
+     * have to make sure people don't get the same Secret Santa more
+     * frequently than once every 3 years and combining the shift with
+     * the 3 year check could get messy. */
+
+    let newPartner = 0;
+
+    for ( let i = 0; i < familiaSize; i++ ) {
+      newPartner = findPartner( family, i );
+      if ( newPartner < 0 ) {
+        console.log( ' Help! Could not find a partner for ' + familias[family][i].santa );
+      } else {
+        assignPartner( family, i, newPartner );
+      }
+    }
+
+    showSecretSantaAssignments(family);
+
+    cleanupHistory( family );
   }
 }
